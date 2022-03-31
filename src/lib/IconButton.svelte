2f9d908d4f09d54e86b54cf0c812809b3abebb8e
Robin Wong <chaowang0313@gmail.com>
2022-04-01 07:01:32 +0800

    feat: minor visual adjustment

--- src/lib/IconButton.svelte
+++ src/lib/IconButton.svelte
@@ -16,6 +16,6 @@
 </script>
 
 <button type="button" on:click class={`flex w-full flex-col items-center justify-center px-2 py-4 rounded-md bg-opacity-10 focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors ${variantClassName[variant]} ${disabledClassName} ${className}`}>
-    <Icon stroke="none" data={icon} width="48" height="48"></Icon>
+    <Icon stroke="none" data={icon} width="32" height="32"></Icon>
     <span class="mt-2 text-base font-normal">{label}</span>
 </button>
